e0fc686592574ae4ad913408d7da69f322eff3a0
stoftot <christoffer.gruberg@gmail.com>
2022-02-07 12:51:29 +0100

    signup og login stuff

--- progEksamensProjekt/src/main/java/org/openjfx/progeksamensprojekt/LoginController.java
+++ progEksamensProjekt/src/main/java/org/openjfx/progeksamensprojekt/LoginController.java
@@ -7,8 +7,14 @@ package org.openjfx.progeksamensprojekt;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.openjfx.databaseRepository.SecurityMethods;
+import org.openjfx.databaseRepository.UserDatabaseMethods;
 
 /**
  * FXML Controller class
@@ -17,25 +23,53 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
+    @FXML
+    private TextField textFieldUsername;
+    @FXML
+    private PasswordField passwordFieldPassword;
+    @FXML
+    private Text textErrorMessage;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }  
-    
-    @FXML
-    private void exit(){
-        System.exit(0);
-    }
-    @FXML
-    private void signup() throws IOException{
-        App.setRoot("signup");
-    }
-    @FXML 
-    private void openPrimary() throws IOException{
-        App.setRoot("primary");
     }
     
+    
+    @FXML
+    private void login(ActionEvent event) throws Exception {
+        textErrorMessage.setText("");
+
+        //check if user exist
+        UserDatabaseMethods userDatabasemethods = new UserDatabaseMethods();
+        if (userDatabasemethods.cehckForMatchingUser(textFieldUsername.getText())) {
+
+            //check if user and passowrd match
+            SecurityMethods securityMethods = new SecurityMethods();
+            if (userDatabasemethods.checkForMatchingPassword(textFieldUsername.getText(),
+                    securityMethods.hexString(passwordFieldPassword.getText()))) {
+
+                App.setLoggedInUser(userDatabasemethods.getoggedInUser(textFieldUsername.getText()));
+                App.setRoot("mainScreen");
+                //System.out.println("logged in");
+
+            } else {
+                textErrorMessage.setText("user dosen't exist or password dont match");
+            }
+        } else {
+            textErrorMessage.setText("user dosen't exist or password dont match");
+        }
+    }
+
+    @FXML
+    private void exit(ActionEvent event) {
+    }
+
+
+    @FXML
+    private void signup(ActionEvent event) {
+    }
 }
--- progEksamensProjekt/src/main/java/org/openjfx/progeksamensprojekt/SignupController.java
+++ progEksamensProjekt/src/main/java/org/openjfx/progeksamensprojekt/SignupController.java
@@ -7,8 +7,17 @@ package org.openjfx.progeksamensprojekt;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.openjfx.classes.User;
+import org.openjfx.databaseRepository.SecurityMethods;
+import org.openjfx.databaseRepository.UserDatabaseMethods;
 
 /**
  * FXML Controller class
@@ -17,13 +26,92 @@ import javafx.fxml.Initializable;
  */
 public class SignupController implements Initializable {
 
+    @FXML
+    private TextField textFieldUsername;
+    @FXML
+    private Text textErroMessage;
+    @FXML
+    private PasswordField passwordFieldPassword;
+    @FXML
+    private PasswordField passwordFieldrepeatPassword;
+    @FXML
+    private TextField textFieldEmail;
+    @FXML
+    private TextField textFieldName;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    private void creatUser(ActionEvent event) throws Exception {
+        textErroMessage.setText("");
+
+        //Check if all fields is filled
+        if (!textFieldUsername.getText().isBlank()
+                && !passwordFieldPassword.getText().isBlank()
+                && !passwordFieldrepeatPassword.getText().isBlank()
+                && !textFieldEmail.getText().isBlank()) {
+
+            //Check if user already exist
+            UserDatabaseMethods userDatabasemethods = new UserDatabaseMethods();
+            if (!userDatabasemethods.cehckForMatchingUser(textFieldUsername.getText())) {
+
+                //Check if password have a special and uppercase character and at least 8 carachters long
+                boolean passwordMeetsRequirements = false;
+                char[] passwordChars = passwordFieldPassword.getText().toCharArray();
+
+                Pattern pattern = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(passwordFieldPassword.getText());
+
+                //length
+                if (passwordFieldPassword.getText().length() >= 8) {
+                    //special caracahter
+                    if (matcher.find()) {
+                        //upper case carachter
+                        for (int i = 0; i < passwordChars.length; i++) {
+                            if (Character.isUpperCase(passwordChars[i])) {
+                                passwordMeetsRequirements = true;
+                                break;
+                            }
+                        }
+                        //passwords is identicel
+                        if (passwordMeetsRequirements) {
+                            if (passwordFieldPassword.getText().equals(passwordFieldrepeatPassword.getText())) {
+
+                                SecurityMethods securityMethods = new SecurityMethods();
+
+                                userDatabasemethods.createUser(new User(textFieldUsername.getText(),
+                                        securityMethods.hexString(passwordFieldPassword.getText()),
+                                        textFieldName.getText(), textFieldEmail.getText()));
+
+                                App.setLoggedInUser(userDatabasemethods.getoggedInUser(textFieldUsername.getText()));
+                                App.setRoot("Sorting");
+                            } else {
+                                textErroMessage.setText("Password meets requirements, but donsen't match");
+                            }
+                        } else {
+                            textErroMessage.setText("Password is missing a uppercase character");
+                        }
+                    } else {
+                        textErroMessage.setText("Password is missing a special character");
+                    }
+                } else {
+                    textErroMessage.setText("Password needs to be at least 8 characters long");
+
+                }
+            } else {
+                textErroMessage.setText("User already exist");
+            }
+        } else {
+            textErroMessage.setText("All fields need to be filled");
+        }
+
+    }
+    
     @FXML
     private void exit(){
         System.exit(0);
